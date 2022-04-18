6be278da1efb66e20c0054a2704f3bc7d4e856e4
Success Ibekwe <Successibekwe047@gmail.com>
2022-04-18 12:36:09 -0800

    added landing page

---
+++ MB_Landing_Page/index.js
@@ -0,0 +1,12 @@
+// const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+// const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+// const navbar = document.querySelector("[data-navbar]");
+
+
+// const navElemArr = [navOpenBtn, navCloseBtn];
+
+// for (let i = 0; i < navElemArr.length; i++) {
+//     navElemArr[i].addEventListener("click", function () {
+//         navbar.classList.toggle("active");
+//     })
+// }
